6edf1c96c10e9785ebeaf28f39f1b183649bb26c
Elina Semenko <lavrextra@gmail.com>
2020-10-21 19:24:12 +0300

    working with api

---
+++ test-app-github/Model/Client.swift
@@ -0,0 +1,9 @@
+//
+//  Client.swift
+//  test-app-github
+//
+//  Created by Элина on 21.10.2020.
+//  Copyright © 2020 Элина. All rights reserved.
+//
+
+import Foundation
---
+++ test-app-github/Utilities/APIparts.swift
@@ -0,0 +1,12 @@
+//
+//  APIparts.swift
+//  test-app-github
+//
+//  Created by Элина on 21.10.2020.
+//  Copyright © 2020 Элина. All rights reserved.
+//
+
+import Foundation
+
+let clientID = "3ddef67466e95e87ba68"
+let clientSecret = "0f74a286ec5b2102338cecb15c686dcdd83f5995"
